deb74d848246110e93671647adf21db9312c375a
DevPaski <71769140+DevPaski@users.noreply.github.com>
2022-01-24 00:31:24 +0100

    PracticaRSA 1st Commit
    To do:
    - Send information from Encrypt to Decrypt Form
    - Convertion from Encrypt to Decrypt

---
+++ PracticaRSA/frmDesencriptar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaRSA
+{
+    public partial class frmDesencriptar : Form
+    {
+        string keyName;
+
+        private static CspParameters cspp = new CspParameters();
+        private static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspp);
+
+        public static string folderKey;
+
+        public static string EncryptedMessage;
+
+        public frmDesencriptar()
+        {
+            InitializeComponent();
+
+            keyName = tbx_container.Text;
+
+            CryptoRSA(keyName);
+        }
+
+        private void CryptoRSA(string nomClau)
+        {
+            cspp.KeyContainerName = nomClau;
+
+            rsa.PersistKeyInCsp = true;
+        }
+
+        private void btn_routeXML_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fileExplorer = new FolderBrowserDialog();
+
+            if(fileExplorer.ShowDialog() == DialogResult.OK)
+            {
+                tbx_routeXML.Text = fileExplorer.SelectedPath + "\\" + tbx_container.Text + ".xml";
+                folderKey = tbx_routeXML.Text;
+            }
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            string publicKey = rsa.ToXmlString(false);
+            string privateKey = rsa.ToXmlString(true);
+
+            File.WriteAllText(tbx_routeXML.Text, publicKey);
+
+            MessageBox.Show("Keys Generated!");
+        }
+    }
+}
---
+++ PracticaRSA/frmEncriptar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace PracticaRSA
+{
+    public partial class frmEncriptar : Form
+    {
+        private static RSACryptoServiceProvider rsaEnc = new RSACryptoServiceProvider();
+        string xmlKey;
+        
+
+        public frmEncriptar()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_obtainKey_Click(object sender, EventArgs e)
+        {
+            xmlKey = File.ReadAllText(frmDesencriptar.folderKey);
+            rsaEnc.FromXmlString(xmlKey);
+        }
+
+        private void btn_showKey_Click(object sender, EventArgs e)
+        {
+            tbx_pubkey.Text = xmlKey;
+        }
+
+        private void btn_encrypt_Click(object sender, EventArgs e)
+        {
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            byte[] dataToEncrypt = ByteConverter.GetBytes(tbx_original.Text);
+
+            byte[] dataEncrypted = rsaEnc.Encrypt(dataToEncrypt, false);
+            tbx_crypted.Text = BitConverter.ToString(dataEncrypted);
+            //encryptedData = rsaEnc.Encrypt(dataToEncrypt, false);
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            frmDesencriptar.EncryptedMessage = this.tbx_crypted.Text;
+        }
+    }
+}
